93e0732ce10e0a8a0a0e020d40f16cd6de3f67e5
mightcell <273606826@qq.com>
2023-02-04 20:33:37 +0800

    准备工作

---
+++ spring_security_quick_start/src/main/java/com/mightcell/SecurityApplication.java
@@ -0,0 +1,12 @@
+package com.mightcell;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
