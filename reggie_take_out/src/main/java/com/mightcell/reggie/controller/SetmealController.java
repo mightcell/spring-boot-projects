51df27a1d7a131c3f6342eecdc95f0241f411bd9
mightcell <273606826@qq.com>
2023-02-06 20:18:18 +0800

    套餐展示

--- reggie_take_out/src/main/java/com/mightcell/reggie/controller/SetmealController.java
+++ reggie_take_out/src/main/java/com/mightcell/reggie/controller/SetmealController.java
@@ -91,4 +91,20 @@ public class SetmealController {
         return R.success("套餐数据删除成功");
     }
 
+    /**
+     * 查询套餐信息
+     * @param setmeal
+     * @return 套餐列表信息
+     */
+    @GetMapping("/list")
+    public R<List<Setmeal>> list(Setmeal setmeal) {
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper
+                .eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId())
+                .eq(setmeal.getStatus() != null, Setmeal::getStatus, setmeal.getStatus())
+                .orderByDesc(Setmeal::getUpdateTime);
+        List<Setmeal> list = setmealService.list(queryWrapper);
+        return R.success(list);
+    }
+
 }
